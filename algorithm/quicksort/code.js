033844de5f9c3a075cd17fc945a8e02234abaf11
developer2006 <developer2006@outlook.com>
2018-07-17 18:04:17 +0800

    Tab to spaces

--- algorithm/quicksort/code.js
+++ algorithm/quicksort/code.js
@@ -1,17 +1,17 @@
 function quickSort(items) {
-	if (items.length <= 1) return items;
-	let pivotIndex = Math.floor(items.length / 2);
-	let pivot = items.splice(pivotIndex, 1)[0];
-	let [left, right] = [[],[]];
-	for (let i = 0; i < items.length; i++) {
-		if (items[i] > pivot) right.push(items[i]);
-		else left.push(items[i]);
-	}
-	return quickSort(left).concat([pivot], quickSort(right));
+  if (items.length <= 1) return items;
+  let pivotIndex = Math.floor(items.length / 2);
+  let pivot = items.splice(pivotIndex, 1)[0];
+  let [left, right] = [[],[]];
+  for (let i = 0; i < items.length; i++) {
+    if (items[i] > pivot) right.push(items[i]);
+    else left.push(items[i]);
+  }
+  return quickSort(left).concat([pivot], quickSort(right));
 }
 
 (() => {
-	let array = [2,1,6,3,7,4,9,8,5,0,2,1,6,3,7,4,9,8,5,0];
-	console.log(`Before: ${array}`);
-	console.log(` After: ${quickSort(array)}`);
+  let array = [2,1,6,3,7,4,9,8,5,0,2,1,6,3,7,4,9,8,5,0];
+  console.log(`Before: ${array}`);
+  console.log(` After: ${quickSort(array)}`);
 })();
