6d7517937638ba94a045356b6473a91b793bb269
developer2006 <developer2006@outlook.com>
2018-08-28 17:32:03 +0800

    Update

--- js/destructuring assignment/s1.js
+++ js/destructuring assignment/s1.js
@@ -15,3 +15,13 @@ console.log(a,b,c,d,e,f,g,h,i,j,k);
 //解构赋值允许指定默认值。
 let [foo = 123] = [];
 console.log(foo);
+
+let fun1 = ({a = 1, b = 2} = {}) => {
+    console.log(a + b);
+}
+fun1()
+
+let fun2 = ([x = 1] = []) => {
+    console.log(x);
+}
+fun2()
