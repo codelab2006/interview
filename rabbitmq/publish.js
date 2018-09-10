516b6b8575852422f164cc880d0d6dd1c1bcf7b0
developer2006 <developer2006@outlook.com>
2018-09-10 21:22:57 +0800

    Add ...

---
+++ rabbitmq/publish.js
@@ -0,0 +1,12 @@
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', (err, conn) => {
+    conn.createChannel((err, ch) => {
+        let ex = 'logs';
+        let msg = 'Hello World';
+        ch.assertExchange(ex, 'fanout', {durable: false});
+        ch.publish(ex, '', new Buffer(msg));
+        console.log(" [x] Sent %s", msg);
+    });
+    setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
